e9e3d2973683eda6886a094901feb076fede5111
mingreen0107 <71gnos@naver.com>
2023-05-21 22:46:29 +0900

    초기 커밋.

---
+++ C1/b10989.c
@@ -0,0 +1,36 @@
+// 오름차순으로 정렬하기
+#include <stdio.h>
+/*#include <stdlib.h>
+
+int compare(const void* arr1, const void* arr2)
+{
+	// 길이 구분
+	if (((int*)arr1) > ((int*)arr2))
+		return 1;
+	else if (((int*)arr1) < ((int*)arr2))
+		return -1;
+	// 대소문자 구분
+	else return 0;
+}
+
+int main() {
+	int N;
+	int i;
+
+	scanf("%d", &N);
+	int* arr = (int*)malloc(sizeof(int) * N);
+
+	for (i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+	}
+	qsort(arr, N, sizeof(int), compare);
+	for (i = 0; i < N; i++) {
+		scanf("%d\n", &arr[i]);
+	}
+
+	return 0;
+}*/
+
+int main() {
+	printf("d");
+}
